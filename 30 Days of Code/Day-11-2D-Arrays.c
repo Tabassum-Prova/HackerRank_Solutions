99be68e49c2e84ca6d01c122ab50aeb4f5fd90eb
Prova <tabassumprova326@gmail.com>
2021-08-03 06:30:59 +0600

    day 11 done

--- 30 Days of Code/Day-11-2D-Arrays.c
+++ 30 Days of Code/Day-11-2D-Arrays.c
@@ -2,7 +2,7 @@
 int main(){
 
     int arr[6][6];
-    int i, j;
+    int i, j, sum=0, max=0;
 
     for(i=0; i<6; i++){
         for(j=0; j<6; j++){
@@ -10,11 +10,16 @@ int main(){
         }
     }
 
-    for(i=0; i<6; i++){
-        for(j=0; j<6; j++){
-            printf("%d", arr[i][j]);
+    for(i=1; i<5; i++){
+        for(j=1; j<5; j++){
+            sum = arr[i][j]+arr[i-1][j-1]+arr[i-1][j+1]+arr[i+1][j-1]+arr[i+1][j+1]+arr[i+1][j]+arr[i-1][j];
+            if(sum>max){
+                max=sum;
+            }
         }
     }
+
+    printf("%d", max);
 
     return 0;
 }
